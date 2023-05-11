3e733c3f4953cce93e04b34bc59a925fc7f55961
Joshua Treudler <joshua@treudler.net>
2023-05-11 13:21:51 +0000

    init

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<title>Treudler Online</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+<style type="text/css">
+	body {
+		font-family: Arial;
+		font-size: 12px;
+}
+</style>
+</head>
+<body>
+	<br>
+	<br>
+	<br>
+	<table width="730" style="margin:0 auto">
+		<tbody><tr>
+			<td width="380"><img src="//cdn.treudler.net/web/logo/sized.png"></td>
+			<br>
+			<td rowspan="2" style="border: #dedede">
+				<div style="border: 2px solid #dedede; padding: 10px">
+					<strong>I am:</strong><br>
+					Joshua Treudler<br>
+                    trading as Treudler Online<br>
+					Hahnenkammstraße 9<br>
+					60388 Frankfurt am Main<br>
+					DE Germany<br>
+					<br>
+					<strong>Contact:</strong><br>
+					+49 6182 8499819<br>
+					hello<at>treudler<dot>net<br>
+				</td>
+		</tr>
+		<tr>
+			<!-- td valign="top">
+				<div style="border: 2px solid #dedede; padding: 10px">
+					<strong>Our specialities are:</strong><br>
+					<ul>
+					<li>Linux Server management</li>
+                    <li>Data hosting and processing operations</li>
+					</ul>
+					<strong>What we offer:</strong>
+					<ul>
+				    <li>24 / 7 Support</li>
+				    <li>High Security Guidelines</li>
+					<li>Water-Cooled Servers</li>
+				    <li>Regular Backups</li>
+				    <li>Offsite Backups</li>
+					<li>Disaster Recovery</li>
+				    <li>Competent and friendly team</li>
+				    </ul>
+					<p>Get in touch with us and we will send you a personal quote.</p>
+				</div>
+			</td --!>
+		</tr>
+	</tbody></table>
+    <!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.treudler.net/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//analytics.treudler.net/matomo.php?idsite=2&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
