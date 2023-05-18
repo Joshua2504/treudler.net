cd3e1e62c6391597c1f4c800703169acd0fa9a7b
Joshua Treudler <joshua@treudler.net>
2023-05-18 11:37:45 +0200

    diverse changes

--- index.php
+++ index.php
@@ -1,83 +1,93 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<title>Joshua Treudler - Home</title>
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-<style type="text/css">
-	body {
-		font-family: Arial;
-		font-size: 12px;
-}
-</style>
-</head>
-<body>
-	<br>
-	<br>
-	<br>
-	<table width="730" style="margin:0 auto">
-		<tbody><tr>
-			<td width="380"><img src="//cdn.treudler.net/web/logo/sized.png"></td>
-			<br>
-			<td rowspan="2" style="border: #dedede">
-				<div style="border: 2px solid #dedede; padding: 10px">
-					<strong>I am:</strong><br>
-					Joshua Treudler<br>
-                    (formerly trading as Treudler Online)<br>
-					<br>
-					<br>
-					<strong>Imprint:</strong><br>
-					Hahnenkammstraße 9<br>
-					60388 Frankfurt am Main<br>
-					DE Germany<br>
-					<br>
-					<strong>Contact:</strong><br>
-					<s>+49 6182 8499819</s><br>
-					helloATtreudlerDOTnet<br>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>Joshua Treudler - Home</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+	<style type="text/css">
+		body {
+			font-family: Arial;
+			font-size: 14px;
+	}
+	</style>
+	</head>
+	<body>
+		<br>
+		<br>
+		<br>
+		<table width="730" style="margin:0 auto">
+			<tbody><tr>
+				<td width="380"><img src="https://cdn.treudler.net/web/logo/sized.png"></td><br>
+				<br>
+				<td rowspan="2" style="border: #dedede">
+					<div style="border: 2px solid #dedede; padding: 10px">
+						<strong>I am:</strong><br>
+						Joshua Treudler<br>
+						(formerly trading as Treudler Online)<br>
+						<br>
+						<br>
+						<strong>Imprint:</strong><br>
+						Hahnenkammstraße 9<br>
+						60388 Frankfurt am Main<br>
+						DE Germany<br>
+						<br>
+						<strong>Contact:</strong><br>
+						<s>+49 6182 8499819</s><br>
+						helloATtreudlerDOTnet<br>
+					</td>
+			</tr>
+			<tr>
+				<td valign="top">
+					<div style="border: 2px solid #dedede; padding: 10px">
+						<strong>My Servers:</strong><br>
+						<ul>
+						<li>Oracle Always Free (<a href="https://netdata-orakel.status.treudler.net" target="_blank">netdata</a>)</li>
+						<li>PH24 (<a href="https://netdata-ph24.status.treudler.net" target="_blank">netdata</a>)<</li>
+						</ul>
+						<strong>Services hosted by me:</strong><br>
+						<ul>
+						<li><a href="https://cor-forum.de" target="_blank">cor-forum.de</a> 
+							<ul>
+							<li><a href="https://maps.cor-forum.de" target="_blank">maps</a></li>
+							<li><a href="https://beta.cor-forum.de" target="_blank">beta</a></li>
+							<li><a href="https://old.cor-forum.de" target="_blank">old &#x2020;</a></li>
+							<li><a href="https://usercontent.cor-forum.de" target="_blank">usercontent &#x2020;</a></li>
+								
+							</ul>
+							<li>
+								<a href="https://cdn.treudler.net" target="_blank">cdn</a></li>
+							<li>	<a href="https://cdn2.treudler.net" target="_blank">cdn2 &#x2020;</a>
+							</li>
+						<li>PiHole on <b>130.61.219.48</b></li>
+						</ul>
+						<strong>Monitoring:</strong>
+						<ul>
+							<li><img height="14px" src="https://gatus.status.treudler.net/api/v1/endpoints/treudler-web_smokeping-treudler-net/health/badge.svg" /><a href="https://smokeping.treudler.net/" target="_blank">Smokeping</a></li>
+							<li><a href="https://gatus.status.treudler.net/" target="_blank">Gatus</a></li>
+							<li><a href="https://kuma.status.treudler.net/" target="_blank">Uptime Kuma</a></li>
+						</ul>
+						<strong>Archiving:</strong>
+						<ul>
+							<li><a href="https://archivebox.treudler.net/" target="_blank">ArchiveBox</a></li>
+	
+						</ul>
+						<p>Feel free to get in touch with me.</p>
+					</div>
 				</td>
-		</tr>
-		<tr>
-			<td valign="top">
-				<div style="border: 2px solid #dedede; padding: 10px">
-					<strong>My Servers:</strong><br>
-					<ul>
-					<li>Oracle Always Free</li>
-					<li>PH24</li>
-					</ul>
-					<strong>Services hosted by me:</strong><br>
-					<ul>
-					<li><a href="https://cor-forum.de" target="_blank">cor-forum.de</a> (with <a href="https://maps.cor-forum.de" target="_blank">maps</a>, <a href="https://usercontent.cor-forum.de" target="_blank">usercontent &#x2020;</a> and <a href="https://old.cor-forum.de" target="_blank">old &#x2020;</a>)</li>
-					<li><a href="https://cdn.treudler.net" target="_blank">cdn</a> and <a href="https://cdn2.treudler.net" target="_blank">cdn2 &#x2020;</a></li>
-					<li>PiHole on <b>130.61.219.48</b></li>
-					</ul>
-					<strong>Monitoring:</strong>
-					<ul>
-						<li><img src="https://gatus.status.treudler.net/api/v1/endpoints/treudler-web_smokeping-treudler-net/health/badge.svg" /><a href="https://smokeping.treudler.net/" target="_blank">Smokeping</a></li>
-						<li><a href="https://gatus.status.treudler.net/" target="_blank">Gatus</a></li>
-						<li><a href="https://kuma.status.treudler.net/" target="_blank">Uptime Kuma</a></li>
-					</ul>
-					<strong>Archiving:</strong>
-					<ul>
-						<li><a href="https://archivebox.treudler.net/" target="_blank">ArchiveBox</a></li>
-
-					</ul>
-					<p>Feel free to get in touch with me.</p>
-				</div>
-			</td>
-		</tr>
-	</tbody></table>
-    <!-- Matomo -->
-<script type="text/javascript">
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//analytics.treudler.net/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '2']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//analytics.treudler.net/matomo.php?idsite=2&amp;rec=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Matomo Code -->
+			</tr>
+		</tbody></table>
+	<!-- Matomo -->
+	<script>
+		var _paq = window._paq = window._paq || [];
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(["setCookieDomain", "*.treudler.net"]);
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+		  var u="//analytics.treudler.net/";
+		  _paq.push(['setTrackerUrl', u+'matomo.php']);
+		  _paq.push(['setSiteId', '2']);
+		  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	  </script>
+	  <noscript><p><img src="//analytics.treudler.net/matomo.php?idsite=2&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+	  <!-- End Matomo Code -->
